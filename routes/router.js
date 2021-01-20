6b304fb0cba08b5ee693744d45dc60eb6a172a7d
Hillary T <htong0001@gmail.com>
2021-01-20 03:48:41 +0000

    messaging works!!!

--- routes/router.js
+++ routes/router.js
@@ -23,9 +23,27 @@ router.get("/read-yourmessage", userMiddleware.isLoggedIn, (req, res, next) => {
 
 });
 
-router.post("/post-message", userMiddleware.isLoggedIn, (req, res, next) => {
-
-});
+mongo.initialize(dbName, msgCollection, function(msgColl) {
+  msgColl.find().toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result)
+  });
+  router.post("/post-message", userMiddleware.isLoggedIn, (req, res, next) => {
+    console.log("about to post message");
+    const message = req.body;
+    msgColl.insertOne(message, (error, result) => {
+      if (error) {
+        throw error;
+        return res.status(400).send({
+          msg: error,
+        });
+      }
+      return res.status(201).send({
+        msg: "Message Sent!",
+      });
+    })
+  });
+})
 
 router.post("/like-message", userMiddleware.isLoggedIn, (req, res, next) => {
 
@@ -36,104 +54,108 @@ router.post("/follow-user", userMiddleware.isLoggedIn, (req, res, next) => {
 });
 
 mongo.initialize(dbName, userCollection, function(userColl) { // successCallback
-    // get all items
-    userColl.find().toArray(function(err, result) {
-        if (err) throw err;
-          console.log(result);
-    });
-
-router.post("/sign-up", userMiddleware.validateRegister, (req, res, next) => {
- const item = req.body;
- console.log("Proceed to find username");
- userColl.find({username: req.body.username}).toArray((error, result) => {
-    console.log(result)
-    if (result.length > 0) {
-      console.log("username is in use!")
-      return res.status(409).send({
-        msg: "This username is already in use!",
-      });
-    } else {
-      console.log("username is not in use")
-      bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-          return res.status(500).send({
-            msg: err,
-          });
-        } else {
-          console.log("Proceed to insert" + item);
-           userColl.insertOne(item, (error, result) => {
-            if (error) {
-              throw error;
-              return res.status(400).send({
-                msg: error,
-              });
-            }
-            return res.status(201).send({
-              msg: "Registered!",
-            });
-          });
-        }
-      });
-    }
+  // get all items
+  userColl.find().toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
   });
-})
+
+  router.post("/sign-up", userMiddleware.validateRegister, (req, res, next) => {
+    console.log("Proceed to find username");
+    userColl.find({ $or: [{ email: req.body.email }, { username: req.body.username }] }).toArray((error, result) => {
+      console.log(result)
+      if (result.length > 0) {
+        console.log("username or email is in use!")
+        return res.status(409).send({
+          msg: "This username or email is already in use!",
+        });
+      } else {
+        console.log("username is not in use")
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).send({
+              msg: err,
+            });
+          } else {
+            let newPass = {
+               email:req.body.email,
+               username:req.body.username,
+               password:hash
+            }
+            console.log("Proceed to insert" + newPass);
+            userColl.insertOne(newPass, (error, result) => {
+              if (error) {
+                throw error;
+                return res.status(400).send({
+                  msg: error,
+                });
+              }
+              return res.status(201).send({
+                msg: "Registered!",
+              });
+            });
+          }
+        });
+      }
+    });
+  })
 });
 
 mongo.initialize(dbName, userCollection, function(userColl) { // successCallback
-    // get all items
-    userColl.find().toArray(function(err, result) {
-        if (err) throw err;
-          console.log(result);
-    });
+  // get all items
+  userColl.find().toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
 
-router.post("/login", (req, res, next) => {
-console.log("entering/logging in with " + req.body.username);
-  console.log(req.body);
-   userColl.find({username: req.body.username}).toArray((err, result) => {
-    // user does not exists
-    if (err) {
-      throw err;
-      return res.status(400).send({
-        msg: err,
-      });
-    }
-    if (!result.length) {
-      return res.status(401).send({
-        msg: "You don't exist",
-      });
-    }
-    // check password
-    bcrypt.compare(req.body.password, result[0]["password"], (bErr, bResult) => {
-      // wrong password
-      if (bErr) {
-        throw bErr;
+  router.post("/login", (req, res, next) => {
+    console.log("entering/logging in with " + req.body.username);
+    console.log(req.body);
+    userColl.find({ username: req.body.username }).toArray((err, result) => {
+      // user does not exists
+      if (err) {
+        throw err;
+        return res.status(400).send({
+          msg: err,
+        });
+      }
+      if (!result.length) {
         return res.status(401).send({
-          msg: "Password is incorrect!",
+          msg: "You don't exist",
         });
       }
-      if (bResult) {
-        const token = jwt.sign(
-          {
-            username: result[0].username,
-            userId: result[0].id,
-          },
-          "SECRETKEY",
-          {
-            expiresIn: "7d",
-          }
-        );
-        //userColl.updateOne({})
-        return res.status(200).send({
-          msg: "Logged in!",
-          token,
-          user: result[0],
+      // check password
+      bcrypt.compare(req.body.password, result[0]["password"], (bErr, bResult) => {
+        // wrong password
+        if (bErr) {
+          throw bErr;
+          return res.status(401).send({
+            msg: "Password is incorrect!",
+          });
+        }
+        if (bResult) {
+          const token = jwt.sign(
+            {
+              username: result[0].username,
+              userId: result[0].id,
+            },
+            "SECRETKEY",
+            {
+              expiresIn: "7d",
+            }
+          );
+          //userColl.updateOne({})
+          return res.status(200).send({
+            msg: "Logged in!",
+            token,
+            user: result[0],
+          });
+        }
+        return res.status(401).send({
+          msg: "Username or password is incorrect!",
         });
-      }
-      return res.status(401).send({
-        msg: "Username or password is incorrect!",
       });
     });
   });
-});
 })
 module.exports = router;
